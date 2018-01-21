e371772918e289b2fbc45c087bcf0d321f749582
Yasuo Hayashibara <yasuo.hayashibara@it-chiba.ac.jp>
2018-01-21 23:54:36 +0900

    add printf

--- F746_controller/Src/rs485.h
+++ F746_controller/Src/rs485.h
@@ -16,15 +16,35 @@ public:
   
   char putc(int c)
   {
-    tx_buf[0] = c;
-    return HAL_UART_Transmit_DMA(_huart, tx_buf, 1) == HAL_OK ? c : EOF;
+    _tx_buf[0] = c;
+    return HAL_UART_Transmit_DMA(_huart, _tx_buf, 1) == HAL_OK ? c : EOF;
   }
   
   int getc()
   {
-    return HAL_UART_Receive_DMA(_huart, rx_buf, 1) == HAL_OK ? rx_buf[0] : EOF;
+    return HAL_UART_Receive_DMA(_huart, _rx_buf, 1) == HAL_OK ? _rx_buf[0] : EOF;
   }
   
+  int write(const void* buffer, size_t length)
+  {
+    unsigned char *buf = (unsigned char *)buffer;
+    unsigned char *tx_buf = _tx_buf;
+    for(int i = 0; i < length; i ++){
+      *tx_buf ++ = *buf ++;
+    }
+    return HAL_UART_Transmit_DMA(_huart, _tx_buf, length) == HAL_OK ? length : 0;
+  }
+  
+  int printf(const char* format, ...)
+  {
+    va_list arg;
+    va_start(arg, format);
+    char buf[256];
+    int res = vsprintf(buf, format, arg);
+    va_end(arg);
+    write(buf, res);
+    return res;
+  }
 
 private:
   UART_HandleTypeDef *_huart;
@@ -33,8 +53,8 @@ private:
 
   static const int TX_BUF_SIZE = 16;
   static const int RX_BUF_SIZE = 256;
-  unsigned char tx_buf[TX_BUF_SIZE];
-  unsigned char rx_buf[RX_BUF_SIZE];
+  unsigned char _tx_buf[TX_BUF_SIZE];
+  unsigned char _rx_buf[RX_BUF_SIZE];
 };
 
 #endif
